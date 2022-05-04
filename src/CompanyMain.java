a68fa1c9c8d190e743910559b3203a2ee48622ff
Maciej <maciej.jurek7@gmail.com>
2022-05-04 10:45:48 +0200

    add CompanyMain, Employee and Manager classes

--- src/CompanyMain.java
+++ src/CompanyMain.java
@@ -1,11 +1,28 @@
 import company.Employee;
 import company.Manager;
+import company.Secretary;
 
 public class CompanyMain {
-    public static void main(String[] args) {
-        Employee employeeOne = new Employee("Jan", 200);
-        Manager employeeTwo = new Manager("Bogdan", 250, 50);
+        public static void main(String[] args) {
+            Employee employeeOne = new Employee("Jan", 200, 11);
+            Manager employeeTwo = new Manager("Bogdan", 250, 50, 22);
+            Secretary employeeThree = new Secretary("Ala", 230, 33);
 
 
-    }
+            Employee employeeFour = new Manager("Ola", 250, 50, 44);
+            System.out.println(employeeFour.getSalary());
+//            employeeTwo.show();
+//            employeeThree.makeCall();
+
+            Employee[] employees = new Employee[3];
+            employees[0] = employeeOne;
+            employees[1] = employeeTwo;
+            employees[2] = employeeThree;
+
+            for(Employee e : employees) {
+                e.show();
+                int employeeSalary = e.getSalary();
+                System.out.println(employeeSalary);
+            }
+        }
 }
--- src/company/Manager.java
+++ src/company/Manager.java
@@ -1,7 +1,7 @@
 package company;
 
 public class Manager extends Employee {
-
+/*
     private int bonus;
 
     public Manager(String name, int salary) {
@@ -29,5 +29,33 @@ public class Manager extends Employee {
     public void makeSpeech(String additionalMessage) {
         makeSpeech();
         System.out.println(additionalMessage);
+    }
+    */
+private int bonus;
+
+    public Manager(String name, int salary) {
+        super(name, salary, 0);
+        System.out.println("Wywołuję konstruktor z klasy Manager z argumentami: " + name + " i " + salary);
+    }
+
+    public Manager(String name, int salary, int bonus, int age) {
+        super(name, salary, age);
+        this.bonus = bonus;
+        System.out.println("Wywołuję konstruktor z klasy Manager z argumentami: " + name + " i " + salary + " i " + bonus + " i " + age);
+    }
+
+    public void makeSpeech() {
+        System.out.println("Do pracy!");
+    }
+
+    public void makeSpeech(String additionalMessage) {
+        makeSpeech();
+        System.out.println(additionalMessage);
+    }
+
+    @Override
+    public int getSalary() {
+        // return getSalary() + bonus;
+        return salary + bonus;
     }
 }
